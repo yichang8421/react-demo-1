7be4e46020b13aa0551a09ff07cd04d8f24b07e7
andy8421 <yichang8421@qq.com>
2021-08-14 22:27:27 +0800

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            n: 0,
+        };
+    }
+
+    render() {
+        return (
+            <div>
+
+                <Receptor name={this.state.n}></Receptor>
+
+                <br/>
+                <button onClick={() => {
+                    this.setState(state => {
+                        return {n: state.n + 1}
+                    });
+                    this.setState(state => {
+                        return {n: state.n + 1}
+                    });
+                }}>不带回调参数 +2
+                </button>
+
+                <br/>
+                <br/>
+                <button onClick={() => {
+                    this.setState((state) => {
+                        return {n: state.n + 1}
+                    }, () => {
+                        this.setState(state => {
+                            return {n: state.n + 1}
+                        })
+                    });
+                }}>带回调参数 +2
+                </button>
+            </div>
+        );
+    }
+}
+
+class Receptor extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillReceiveProps(newProps, nextContext) {
+        console.log(`props: ${this.props.name}`);
+        console.log("变成");
+        console.log(`newProps: ${newProps.name}`);
+    }
+
+    render() {
+        return (
+            <div>n: {this.props.name}</div>
+        );
+    }
 }
 
 export default App;
