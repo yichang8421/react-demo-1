44a51f94a0fd45231995677ea7b1ddbb1be03f6a
andy8421 <yichang8421@qq.com>
2021-08-14 22:50:43 +0800

    shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -4,60 +4,33 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            n: 0,
+            n:0
         };
     }
 
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if(nextState.n === this.state.n) return false;
+    }
+
     render() {
+        console.log('rendered');
         return (
             <div>
-
-                <Receptor name={this.state.n}></Receptor>
-
+                n: {this.state.n}
                 <br/>
-                <button onClick={() => {
-                    this.setState(state => {
-                        return {n: state.n + 1}
+                <button onClick={()=>{
+                    this.setState((state)=>{
+                        return {n: state.n + 1};
                     });
-                    this.setState(state => {
-                        return {n: state.n + 1}
+                    this.setState((state)=>{
+                        return {n: state.n - 1};
                     });
-                }}>不带回调参数 +2
-                </button>
-
-                <br/>
-                <br/>
-                <button onClick={() => {
-                    this.setState((state) => {
-                        return {n: state.n + 1}
-                    }, () => {
-                        this.setState(state => {
-                            return {n: state.n + 1}
-                        })
-                    });
-                }}>带回调参数 +2
-                </button>
+                }}>+1</button>
             </div>
         );
+
     }
 }
 
-class Receptor extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentWillReceiveProps(newProps, nextContext) {
-        console.log(`props: ${this.props.name}`);
-        console.log("变成");
-        console.log(`newProps: ${newProps.name}`);
-    }
-
-    render() {
-        return (
-            <div>n: {this.props.name}</div>
-        );
-    }
-}
 
 export default App;
