dfa5c5ee8eddaab72c5c8413c5ca2a8b55c88a41
andy8421 <yichang8421@qq.com>
2021-08-15 21:19:43 +0800

    useEffect simulates componentDidUpdate (排除n第一次渲染情况)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, {useState, useEffect} from 'react'
 
 const App = () => {
-    let [ChildVisable, setChildVisable] = useState(true);
+    const [n, setN] = useState(0);
+
+    const [nUpdateCount, setNUpdateCount] = useState(0);
+
+    useEffect(() => {
+        setNUpdateCount(nUpdateCount => nUpdateCount + 1);
+    }, [n]);
+
+    useEffect(() => {
+        if (nUpdateCount > 1) {
+            console.log('n updeated');
+        }
+    }, [nUpdateCount]);
+
     return (
         <div className="root">
-            {ChildVisable ?
-                <button onClick={() => {
-                    setChildVisable(false);
-                }}>hide</button> :
-                <button onClick={() => {
-                    setChildVisable(true);
-                }}>show</button>}
-            {ChildVisable ? <Child/> : null}
+            n: {n}
+            <hr/>
+            <button onClick={() => {
+                setN(n + 1);
+            }}>+1
+            </button>
         </div>
     );
-};
-
-const Child = () => {
-    useEffect(() => {
-        console.log("Child渲染了(componentDidMount)");
-        return () => {
-            console.log("Child被销毁了(componentWillUnmount)");
-        }
-    },[])
-
-    return (
-        <div>Child</div>
-    );
-};
+}
 
 
 export default App;
