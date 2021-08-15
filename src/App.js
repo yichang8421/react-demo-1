1cd459f077f9c848f9cf38ed3aef5c37cdd48a82
andy8421 <yichang8421@qq.com>
2021-08-16 01:24:45 +0800

    useRef

--- src/App.js
+++ src/App.js
@@ -1,65 +1,25 @@
-import React, {useEffect} from 'react'
-import ReactDOM from "react-dom";
-
-let _state = [];
-let index = 0;
-const myUseState = (initialValue) => {
-    const currentIndex = index;
-
-    _state[currentIndex] = _state[currentIndex] === undefined ? initialValue : _state[currentIndex];
-
-    const setState = (newValue) => {
-        _state[currentIndex] = newValue;
-
-        index = 0;
-        ReactDOM.render(
-            <App/>,
-            document.getElementById('root')
-        );
-    };
-
-    index += 1;
-    return [_state[currentIndex], setState];
-};
+import React, {useRef, useState} from 'react'
 
 const App = () => {
-    const [n, setN] = myUseState(0);
-    const [m, setM] = myUseState(0);
-
-    const useUpdate = (fn, n) => {
-        const [nUpdateCount, setNUpdateCount] = myUseState(0);
-
-        useEffect(() => {
-            setNUpdateCount(nUpdateCount + 1);
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [n]);
-
-        useEffect(() => {
-            if (nUpdateCount > 1) {
-                fn();
-            }
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [nUpdateCount]);
-    }
-
-    useUpdate(() => {
-        console.log('updeted');
-    }, n);
+    const nRef = useRef(0);
+    const updateN = useState(null)[1];
 
     return (
         <div className="root">
-            n: {n}
+            n: {nRef.current}
+
             <hr/>
             <button onClick={() => {
-                setN(n + 1);
+                updateN(nRef.current++);
             }}>+1
             </button>
-            <hr/>
-            m: {m}
+
             <hr/>
             <button onClick={() => {
-                setM(m + 1);
-            }}>+1
+                setTimeout(() => {
+                    console.log(nRef.current);
+                }, 3000)
+            }}>三秒后控制台打印 n
             </button>
         </div>
     );
